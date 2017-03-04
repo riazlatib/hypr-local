67c00fe2304eb6adf540ce723e2466f4cf40a2af
Riaz Latib <riazlatib@gmail.com>
2017-03-04 11:48:40 -0500

    Troubleshooting the map render

--- includes/home.php
+++ includes/home.php
@@ -1,18 +1,16 @@
 
 <?php
-  echo 'what?';
 ?>
 <div id="map"></div>
 
-<!-- Replace the value of the key parameter with your own API key. -->
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&callback=initMap" async defer></script>
-
+<!--/* - JavaScript Section - */ -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAw6mTI47cPQ9j9hYcQFnhagDKYOayRniw&callback=initMap" async defer></script>
 <script>
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
 </script>
