d6903fb5ca2f619f83d99f83a822fd4d1d7d25e5
Riaz Latib <riazlatib@gmail.com>
2017-03-04 20:36:06 -0500

    Still going

--- js/home.js
+++ js/home.js
@@ -65,7 +65,6 @@ function gas() {
     		  console.log('There has been an error: ' + errorThrown);
     		}
       });
-      //https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.feedburner.com%2Forlandosentinel%2Fnews
 		},
 		error: function (errorThrown) {
 		  //error - response
